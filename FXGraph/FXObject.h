83bbda8484b3bc446e7bccb0377baa08835fda57
Andrew Pavlov <pandrew@mail.ru>
2019-06-23 11:11:30 +0900

    Исключены некоторые заголовочные файлы из проекта

--- FXGraph/FXObject.h
+++ FXGraph/FXObject.h
@@ -205,7 +205,6 @@ void WriteStr(CArchive&ar, LPCSTR fmt,...);
 class CFXObject : public CObject
 {
 public:
-	static desctype m_Descriptions;
 	CFXObject(void);
 	DECLARE_SERIAL(CFXObject);
 	virtual void AfterSerialize(){}
